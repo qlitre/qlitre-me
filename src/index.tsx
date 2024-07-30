15fe5d469e924e0c930b22d5ae1d07656a2bd9cc
qlitre <kuri0403@gmail.com>
2024-07-30 22:56:48 +0900

    pagesへの置き換え

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,53 @@
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/cloudflare-workers'
 import { renderer } from './renderer'
-import manifest from '__STATIC_CONTENT_MANIFEST'
 
 const app = new Hono()
 
-app.use('/static/*', serveStatic({ root: './', manifest }))
-app.use('/favicon.ico', serveStatic({ path: './favicon.ico', manifest }))
-app.get('/', renderer({}))
+app.use(renderer)
+
+const Body = () => (
+  <div>
+    <p>Hello, It's Qlitre's site like portfolio.</p>
+    <p>This site is powered by Hono.</p>
+    <h2>About</h2>
+    <ul>
+      <li>Is sunday programmer</li>
+      <li>Working in public limited company</li>
+      <li>Love with <code>柴田聡子</code> and <code>paiko fried rice</code></li>
+    </ul>
+    <h2>Link</h2>
+    <ul>
+      <li>
+        <a href="https://qlitre-dialy.ink/" target="_blank" rel="noopener noreferrer">
+          日記サイト(Hono)
+        </a>
+      </li>
+      <li>
+        <a href="https://qlitre-weblog.com/" target="_blank" rel="noopener noreferrer">
+          プログラミングブログ(Nuxt.js)
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/qlitre" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </li>
+      <li>
+        <a href="https://atcoder.jp/users/Qlitre" target="_blank" rel="noopener noreferrer">
+          AtCoder
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/kuri_tter" target="_blank" rel="noopener noreferrer">
+          X / @kuri_tter
+        </a>
+      </li>
+    </ul>
+  </div>
+);
+
+app.get('/', (c) => {
+  return c.render(<Body></Body>)
+})
+
 export default app
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,74 +1,35 @@
-import { html } from 'hono/html'
-import { MiddlewareHandler } from 'hono'
-
+import { jsxRenderer } from 'hono/jsx-renderer'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 
-export type SiteData = {}
-
-export const body = () => {
-    return (
-        <div><p>Hello, It's Qlitre's site like portfolio.</p>
-            <h2>About</h2>
-            <ul>
-                <li>Is sunday programmer</li>
-                <li>Working in public limited company</li>
-                <li>Love with <code>柴田聡子</code> and <code>paiko fried rice</code></li>
-            </ul>
-            <h2>Link</h2>
-            <ul>
-                <li><a href="https://qlitre-dialy.ink/" target="_blank" rel="noopener noreferrer">日記サイト(Hono)</a></li>
-                <li><a href="https://qlitre-weblog.com/" target="_blank" rel="noopener noreferrer">プログラミングブログ(Nuxt.js)</a></li>
-                <li><a href="https://github.com/qlitre" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                <li><a href="https://atcoder.jp/users/Qlitre" target="_blank" rel="noopener noreferrer">AtCoder</a></li>
-                <li><a href="https://twitter.com/kuri_tter" target="_blank" rel="noopener noreferrer">X / @kuri_tter</a></li>
-            </ul>
-        </div>
-    )
-}
-
-export const renderer = (data: SiteData): MiddlewareHandler => {
-    return async (c) => {
-        const html = Layout(body(), data)
-        return c.html(html)
-    }
-}
-
-export const Layout = (body: JSX.Element, data: SiteData) => {
-    const App = () => {
-        return (
-            <div class="container">
-                <Header />
-                <div>
-                    {body}
-                </div>
-                <Footer />
-            </div>
-        )
-    }
-
-    return html`<!DOCTYPE html>
+export const renderer = jsxRenderer(({ children }) => {
+  return (
     <html>
-        <head>
-            <meta charset="utf-8" />
-            <title>Qlitre.Me</title>
-            <link rel="stylesheet" href="/static/css/style.css" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="description" content="Qlitre's Portfolio" />
-            <meta name="author" content="Qlitre" />
-            <meta property="og:title" content="Qlitre.Me" />
-            <meta property="og:description" content="Qlitre's site,Like Portfolio" />
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://qlitre.me/" />
-            <meta property="og:image" content="https://qlitre.me/static/image/myprof.jpeg" />
-            <meta property="og:site_name" content="Qlitre.Me" />
-            <meta property="og:locale" content="ja_JP" />
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@kuri_tter" />
-            <meta name="twitter:creator" content="@kuri_tter" />
-        </head>
-        <body>
-        ${(<App />)}
-        </body>
-    </html>`
-}
+      <head>
+        <link href="/static/css/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Qlitre's Portfolio" />
+        <meta name="author" content="Qlitre" />
+        <meta property="og:title" content="Qlitre.Me" />
+        <meta property="og:description" content="Qlitre's site,Like Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://qlitre.me/" />
+        <meta property="og:image" content="https://qlitre.me/static/image/myprof.jpeg" />
+        <meta property="og:site_name" content="Qlitre.Me" />
+        <meta property="og:locale" content="ja_JP" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@kuri_tter" />
+        <meta name="twitter:creator" content="@kuri_tter" />
+      </head>
+      <body>
+        <div class="container">
+          <Header />
+          <div>
+            {children}
+          </div>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  )
+})
